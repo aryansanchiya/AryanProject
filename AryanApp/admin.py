3c210a0bbbfd2d67ac51d4c2f1f2c1225381f7ae
aryansanchiya <asanchiya@gmail.com>
2024-05-09 11:39:29 +0530

    Admin Added

--- AryanApp/admin.py
+++ AryanApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Writing,Collobrate
+
+# admin.site.register(Writing)
+# admin.site.register(Collobrate)
+
+@admin.register(Writing)
+class Writing(admin.ModelAdmin):
+    list_display = ('title','date','Author')
+    list_filter = ('date','Author')
+
+@admin.register(Collobrate)
+class Collobrate(admin.ModelAdmin):
+    list_display = ('fullname','company_name','email_address')
